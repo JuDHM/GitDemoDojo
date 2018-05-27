40175d214171369d311db27ac56417833c2ad8c6
carmarlo <carmarlo@bancolombia.com.co>
2018-05-27 13:46:15 -0500

    Se realizan cambios en el HomePage

--- src/main/java/com/ch/gitdojo/introgit/pages/HomePage.java
+++ src/main/java/com/ch/gitdojo/introgit/pages/HomePage.java
@@ -1,6 +1,5 @@
 package com.ch.gitdojo.introgit.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,20 +9,20 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 
-	@FindBy(id="searchInput")
+	@FindBy(id = "searchInput")
 	private WebElement inputTerm;
-	
-	@FindBy(xpath="//*[@id=\"search-form\"]/fieldset/button")
+
+	@FindBy(xpath = "//*[@id=\"search-form\"]/fieldset/button")
 	private WebElement btnSearch;
-	
+
 	public HomePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public ResultPage ejecutarBusqueda(WebDriverWait wait, String term) {
-		wait.until(ExpectedConditions.visibilityOf(inputTerm));
+		wait.until(ExpectedConditions.visibilityOf(btnSearch));
 		inputTerm.sendKeys(term);
-		//wait.until(ExpectedConditions.elementToBeClickable(btnSearch));
+		wait.until(ExpectedConditions.elementToBeClickable(btnSearch));
 		btnSearch.click();
 		return new ResultPage();
 	}
